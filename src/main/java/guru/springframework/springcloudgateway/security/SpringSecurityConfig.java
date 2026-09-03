9a46ed620003058f154178d0404c5cb96371c7cc
Brian Alvarez <brianalvarez2000@gmail.com>
2026-09-02 20:56:29 -0500

    Add Actuator and properties for Kubernetes Readiness and Liveness

--- src/main/java/guru/springframework/springcloudgateway/security/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/springcloudgateway/security/SpringSecurityConfig.java
@@ -1,7 +1,9 @@
 package guru.springframework.springcloudgateway.security;
 
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorFilterChain(HttpSecurity httpSecurity) {
+        return httpSecurity
+                .securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
